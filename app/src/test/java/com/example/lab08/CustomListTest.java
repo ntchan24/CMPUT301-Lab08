3f1875784c55f618cd1f577839195019a066ead6
Nathaniel Chan <nathaniel.chan530@gmail.com>
2026-03-05 17:51:10 -0700

    added failing test testDeleteCity

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,9 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+
 public class CustomListTest {
     @Test
     public void testHasCity(){
@@ -11,5 +14,21 @@ public class CustomListTest {
         list.addCity(calgary);
 
         assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDeleteCity(){
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "Alberta");
+        list.addCity(edmonton);
+        list.deleteCity(edmonton);
+
+        assertFalse(list.hasCity(edmonton));
+
+        assertThrows(NoSuchElementException.class, () -> {
+            list.deleteCity(edmonton);
+        });
+
+
     }
 }
